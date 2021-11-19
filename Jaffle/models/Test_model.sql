c93cad232c74c6b32c417d00752b533dcbae134d
justin-t-cole <justintcole@gmail.com>
2021-11-19 13:39:07 +0000

    Updated files

---
+++ Jaffle/models/Test_model.sql
@@ -0,0 +1,1 @@
+SELECT bldg_id, sum("out.electricity.interior_lighting.energy_consumption") as total_interior_lighting_consumption FROM "defaultdb"."upgrade_0" where county='G5500690' group by bldg_id;
--- Jaffle/models/sample_nrel.sql
+++
@@ -1,3 +0,0 @@
-SELECT bldg_id, sum("out.electricity.interior_lighting.energy_consumption") as total_interior_lighting_consumption 
-FROM "defaultdb"."upgrade_0" where county='G5500690' 
-group by bldg_id;
