4ed2bdc4c9d5869fb6609c0b7c5c8a42a7662f44
justin-t-cole <justintcole@gmail.com>
2021-11-26 22:08:52 +0000

    New Model 1

--- Jaffle/models/test_model1.sql
+++ Jaffle/models/test_model1.sql
@@ -1,1 +1,18 @@
-SELECT bldg_id, sum("out.electricity.interior_lighting.energy_consumption") as total_interior_lighting_consumption FROM "defaultdb"."upgrade_0" where county='G5500690' group by bldg_id;
+With sample_buildings as (
+    SELECT 
+        bldg_id,
+        city,
+        state
+    FROM "defaultdb"."buildings" 
+    group by 1
+)
+/*where county='G5500690' */
+
+Select 
+    sum(out.electricity.clothes_dryer.energy_consumption),
+    bldg_id
+from "defaultdb"."resby_puma_northeast"
+
+
+left join sample_buildings  using (bldg_id)
+
